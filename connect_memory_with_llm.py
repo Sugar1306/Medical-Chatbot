a1c31d1794c92069fbf66c2bca5cf55894664968
Tanishka1306 <144044726+Sugar1306@users.noreply.github.com>
2025-12-10 15:12:44 +0530

    Add files via upload

---
+++ connect_memory_with_llm.py
@@ -0,0 +1,54 @@
+from langchain_groq.chat_models import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads .env values into os.environ
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+
+def load_llm():
+    return ChatGroq(
+        model="llama-3.1-8b-instant",  # Choose a Groq-supported model
+        temperature=0.0,
+        max_tokens=256,
+        max_retries=2,
+    )
+
+# Step 2: Connect LLM with FAISS and Create chain
+
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+Dont provide anything out of the given context
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    return prompt
+
+# Load Database
+DB_FAISS_PATH="vectorstore/db_faiss"
+embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+# Create QA chain
+qa_chain=RetrievalQA.from_chain_type(
+    llm=load_llm(),
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k':3}),
+    return_source_documents=True,
+    chain_type_kwargs={'prompt':set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+)
+
+# Now invoke with a single query
+user_query=input("Write Query Here: ")
+response=qa_chain.invoke({'query': user_query})
+print("RESULT: ", response["result"])
+print("SOURCE DOCUMENTS: ", response["source_documents"])
